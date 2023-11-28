29e1cdd9e5d0d8e8ac5fbda291487c32816fb26c
MrZakaz <adam.staron@vp.pl>
2023-11-28 20:55:23 +0100

    CalculateBMI

--- Calculate BMI.js
+++ Calculate BMI.js
@@ -7,7 +7,13 @@ function bmi(weight, height) {
 
 }
 console.log(bmi(80, 1.80))
-
+// function bmi(weight, height) {
+//
+//     var bmi  = weight/(height*height);
+//
+//     return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+//
+// }
 // test
 //
 // const Test = require('@codewars/test-compat');
