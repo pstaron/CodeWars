ca6dc2b00233ddb81718d5acdf403274d71bc78c
MrZakaz <adam.staron@vp.pl>
2024-05-11 20:09:31 +0200

    msarrayv2

--- msarray.js
+++ msarray.js
@@ -6,14 +6,11 @@ const maxSubArray = (nums) => {
     // iterate through the nums, store sub-problems result
     for(let i = 0; i < nums.length; i++){
         //cumulating answers to the top
-
         //compare currentSum add current number
         //with current number and store the maximum value
         currentSum = Math.max(nums[i], currentSum + nums[i])
-
         //compare maxSum with currentSum and store the greater value
         maxSum = Math.max(currentSum, maxSum)
-
     }
     return maxSum
 }
