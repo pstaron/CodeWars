916521627a872d71c625a2188a6feb20c49be324
MrZakaz <adam.staron@vp.pl>
2024-06-10 22:11:43 +0200

    You only need one - Beginner

---
+++ YouOnlyNeedOne.js
@@ -0,0 +1,18 @@
+function check(a, x) {
+return(a.includes(x))
+}
+console.log(check([66, 101], 66))
+
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+//
+// describe("Basic tests",() =>{
+//     it("Testing for fixed tests", () => {
+//         assert.strictEqual(check([66, 101], 66), true);
+//         assert.strictEqual(check([101, 45, 75, 105, 99, 107], 107), true);
+//         assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+//         assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+//     })
+// })
