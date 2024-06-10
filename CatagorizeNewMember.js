d11efbd5b03be7a892abc91572a26b66498364ad
MrZakaz <adam.staron@vp.pl>
2024-06-10 22:01:31 +0200

    openOrSenior

---
+++ CatagorizeNewMember.js
@@ -0,0 +1,18 @@
+
+function openOrSenior(data) {
+    return data.map((value) => value[0]<55 &&value[1]<7?"Open":"Senior")
+}
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
+
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+//
+// describe("Basic tests", () => {
+//     it("Fixed tests", () => {
+//         assert.deepEqual(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
+//         assert.deepEqual(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
+//         assert.deepEqual(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]),['Senior', 'Open', 'Open', 'Open'])
+//     });
+// });
